902e1c6dfa7a44c026d9ac98759ab42e72196236
Sarah Morris <sarah.morris9@gmail.com>
2018-05-04 13:13:25 -0500

    Continuing to work on pet model

--- server/models/pet.js
+++ server/models/pet.js
@@ -5,6 +5,19 @@ module.exports = (sequelize, DataTypes) => {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
     birth_date: DataTypes.DATEONLY,
-    
-  })
+    breed: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    food_amount: DataTypes.TEXT,
+    food_brand: DataTypes.TEXT,
+    feeding_instruactions: DataTypes.TEXT,
+    notes: DataTypes.TEXT
+  }, {tableName: "pet", timestamps: true});
+  Pet.associate = function(models) {
+    Pet.belongsToMany(models.User, {
+      through: {
+        model: "user_pet",
+        unique: true
+      }
+    })
+  }
 }
