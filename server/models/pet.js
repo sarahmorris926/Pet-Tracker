4394b1a6fdcc267a6d4e0e0dcbe62c9b7226f64c
Sarah Morris <sarah.morris9@gmail.com>
2018-05-07 14:19:18 -0500

    Added foreign keys for event table

--- server/models/pet.js
+++ server/models/pet.js
@@ -1,23 +1,36 @@
-'use strict';
+"use strict";
 
 module.exports = (sequelize, DataTypes) => {
-  var Pet = sequelize.define('Pet', {
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    birth_date: DataTypes.DATEONLY,
-    breed: DataTypes.STRING,
-    gender: DataTypes.STRING,
-    food_amount: DataTypes.TEXT,
-    food_brand: DataTypes.TEXT,
-    feeding_instruactions: DataTypes.TEXT,
-    notes: DataTypes.TEXT
-  }, {tableName: "pet", timestamps: true});
+  var Pet = sequelize.define(
+    "Pet",
+    {
+      first_name: DataTypes.STRING,
+      last_name: DataTypes.STRING,
+      birth_date: DataTypes.DATEONLY,
+      breed: DataTypes.STRING,
+      gender: DataTypes.STRING,
+      food_amount: DataTypes.TEXT,
+      food_brand: DataTypes.TEXT,
+      feeding_instruactions: DataTypes.TEXT,
+      notes: DataTypes.TEXT
+    },
+    { tableName: "pet", timestamps: true }
+  );
   Pet.associate = function(models) {
     Pet.belongsToMany(models.User, {
       through: {
         model: "user_pet",
         unique: true
       }
-    })
-  }
-}
+    });
+    Pet.belongsToMany(models.Vet, {
+      through: {
+        model: "vet_visit",
+        unique: true
+      }
+    });
+    Pet.hasMany(models.Event, {
+      foreignKey: "pet_id"
+    });
+  };
+};
---
+++ server/models/vet_visit.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  var Vet_Visit = sequelize.define('Vet_Visit', {
+    date: DataTypes.DATE,
+    vaccines: DataTypes.STRING,
+    medicine_administered: DataTypes.STRING,
+    notes: DataTypes.TEXT
+  }, {tableName: "vet_visit", timestamps: true});
+}
