bfe500f5be3ca0d03e0954763a34eb2278195535
Sarah Morris <sarah.morris9@gmail.com>
2018-05-07 15:24:10 -0500

    Finished events model with joins

--- server/models/events.js
+++ server/models/events.js
@@ -18,6 +18,9 @@ module.exports = (sequelize, DataTypes) => {
     Events.belongsTo(models.User, {
       foreignKey: "user_id"
     });
+    Events.belongsTo(models.Pet, {
+      foreignKey: "pet_id"
+    });
     Events.belongsTo(models.Event_Type, {
       foreignKey: "event_type_id"
     });
---
+++ server/models/user_pet.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var User_Pet = sequelize.define(
+    "User_Pet",
+    {
+      adoption_date: DataTypes.DATE
+    },
+    { tableName: "user_pet", timestamps: false }
+  );
+};
