88b04cb3f576f30ecb9d60ddbce7fc10cd5bfadf
Sarah Morris <sarah.morris9@gmail.com>
2018-05-09 16:13:46 -0500

    Still working on pet factory

--- client/factories/petFactory.js
+++ client/factories/petFactory.js
@@ -1,5 +1,17 @@
 'use strict';
 
 angular.module("PupTracker").factory("PetFactory", ($q, $http, $rootScope) => {
-  
+  return {
+    createPet(petObj) {
+      return $q((resolve, reject) => {
+        $http.post('/createPet', petObj).then(petData => {
+          console.log('new pet created', petData);
+          resolve(petData.data);
+        });
+      }).catch(err => {
+        reject(err);
+      });
+    },
+
+  }
 })
--- server/routes/pet-route.js
+++ server/routes/pet-route.js
@@ -1,2 +1,10 @@
 'use strict';
 
+const Router = require('express');
+const router = Router();
+
+const {} = require('../controllers/petCtrl.js');
+
+router.post("/functionroute", function);
+
+module.exports = router;
