87fd9d7cbecbaeb2a08e146f8fe1630b6dc55a88
Sarah Morris <sarah.morris9@gmail.com>
2018-05-14 16:14:23 -0500

    Creating pet is working but not automatically added to user

--- client/controllers/petCtrl.js
+++ client/controllers/petCtrl.js
@@ -4,6 +4,8 @@ angular
   .module("PupTracker")
   .controller("PetCtrl", function($scope, PetFactory, $location) {
 
+    $scope.newPet = {};
+
     $scope.changeView = (view) => {
       console.log('view', view);
       $location.path(view);
@@ -16,8 +18,8 @@ angular
     };
 
     $scope.createPet = (petObj) => {
-      PetFactory.createPet().then(petData => {
-        $scope.newPet = petData;
+      PetFactory.createPet($scope.newPet).then(data => {
+        console.log("added pet", data)
       })
     }
   });
