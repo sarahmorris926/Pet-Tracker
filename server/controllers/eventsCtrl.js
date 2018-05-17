da39287c6ffdeee3985f1742755c3ba0b8044a51
Sarah Morris <sarah.morris9@gmail.com>
2018-05-17 11:42:39 -0500

    Cleaned up events

--- server/controllers/eventsCtrl.js
+++ server/controllers/eventsCtrl.js
@@ -16,45 +16,44 @@ const {
 module.exports.getAllEvents = (req, res, next) => {
   if (req.session.passport != undefined) {
     Pet.findOne({
-      where: {id: req.params.id}
+      where: { id: req.params.id }
     })
-    .then(pet => {
-      pet.getEvents()
-      .then(data => {
-        res.status(200).json(data)
+      .then(pet => {
+        pet.getEvents().then(data => {
+          res.status(200).json(data);
+        });
       })
-    })
-    .catch(err => {
-      next(err);
-    })
-  }
+      .catch(err => {
+        next(err);
+      });
+  };
 };
 
 module.exports.getOneEvent = (req, res, next) => {
   if (req.session.passport != undefined) {
     Pet.findOne({
-      where: {id: req.params.id}
+      where: { id: req.params.id }
     })
-    .then(pet => {
-      pet.getEvents().then(data => {
-        res.status(200).json(data);
+      .then(pet => {
+        pet.getEvents().then(data => {
+          res.status(200).json(data);
+        });
       })
+      .catch(err => {
+        next(err);
+      });
+  };
+};
+
+module.exports.createEvent = (req, res, next) => {
+  console.log("REQBODY", req.body);
+  req.body.user_id = req.user.id;
+  Events.create(req.body) // should be (req.body.eventInfo) - eventInfo will be the object with most of the info that is passed in, not including weight or vet
+    .then(addedEvent => {
+      console.log("added event", addedEvent);
+      res.status(200).json(addedEvent);
     })
     .catch(err => {
       next(err);
-    })
-  }
-}
-
-module.exports.createEvent = (req, res, next) => {
-  console.log("REQBODY", req.body)
-  req.body.user_id = req.user.id;
-  Events.create(req.body) // should be (req.body.eventInfo) - eventInfo will be the object with most of the info that is passed in, not including weight or vet
-  .then(addedEvent => {
-    console.log("added event", addedEvent)
-    res.status(200).json(addedEvent);
-  })
-  .catch(err => {
-    next(err);
-  })
-}
+    });
+};
