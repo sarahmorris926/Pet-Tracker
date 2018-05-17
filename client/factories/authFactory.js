c3d7b51af90b1157ac9106db18e89a206dd2eb6b
Sarah Morris <sarah.morris9@gmail.com>
2018-05-16 20:31:31 -0500

    Cleaned up code on factories

--- client/factories/authFactory.js
+++ client/factories/authFactory.js
@@ -1,71 +1,73 @@
 "use strict";
 
-angular
-  .module("PupTracker")
-  .factory("AuthFactory", ($q, $http, $rootScope) => {
-    let currentUser = null;
+angular.module("PupTracker").factory("AuthFactory", ($q, $http, $rootScope) => {
+  let currentUser = null;
 
-    return {
-      createUser(userObj) {
-        return $q((resolve, reject) => {
-          $http.post("/register", userObj).then(userData => {
-            console.log("new user added", userData);
-            currentUser = userData;
-            resolve(userData.data);
-          });
-        }).catch(err => {
-          reject(err);
+  return {
+    createUser(userObj) {
+      return $q((resolve, reject) => {
+        $http.post("/register", userObj).then(userData => {
+          console.log("new user added", userData);
+          currentUser = userData;
+          resolve(userData.data);
         });
-      },
+      }).catch(err => {
+        reject(err);
+      });
+    },
 
-      loginUser(userObj) {
-        console.log("userObj", userObj);
+    loginUser(userObj) {
+      console.log("userObj", userObj);
 
-        return $q((resolve, reject) => {
-          $http.post("/login", userObj).then(user => {
-            currentUser = user;
-            resolve(user.data);
-          });
-        }).catch(err => {
-          reject(err);
+      return $q((resolve, reject) => {
+        $http.post("/login", userObj).then(user => {
+          currentUser = user;
+          resolve(user.data);
         });
-      },
+      }).catch(err => {
+        reject(err);
+      });
+    },
 
-      logoutUser() {
-        return $q((resolve, reject) => {
-            $http.post("/logout").then(user => {
-                console.log("logged out the user")
-                currentUser = null;
-                resolve();
-            });
-        }).catch(err => {
-            reject(err);
+    logoutUser() {
+      return $q((resolve, reject) => {
+        $http.post("/logout").then(user => {
+          console.log("logged out the user");
+          currentUser = null;
+          resolve();
+        });
+      }).catch(err => {
+        reject(err);
+      });
+    },
+
+    getCurrentUser() {
+      return currentUser;
+    },
+
+    setUserStatus() {
+      return $http
+        .get("/status")
+        .then(user => {
+          console.log("user in set user status", user);
+
+          if (user) {
+            currentUser = user.data;
+          } else {
+            currentUser = null;
+          }
+        })
+        .catch(() => {
+          currentUser = null;
         });
     },
 
-      getCurrentUser() {
-        return currentUser;
-      },
+    getCurrentUser() {
+      return currentUser;
+    },
 
-      setUserStatus() {
-        return $http
-          .get("/status")
-          .then(user => {
-            console.log("user in set user status", user);
-
-            if (user) {
-              currentUser = user.data;
-            } else {
-              currentUser = null;
-            }
-          })
-          .catch(() => {
-            currentUser = null;
-          });
-      },
-
-      broadcastUserLogin(user) {
-        $rootScope.$broadcast("handleBroadcast", user);
-      }
-    };
-  });
+    broadcastUserLogin(user) {
+      $rootScope.$broadcast("handleBroadcast", user);
+    }
+  };
+});
