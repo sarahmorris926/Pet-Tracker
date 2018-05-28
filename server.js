29784015dd32b1c26275102cda5a9969d010ea6c
Sarah Morris <sarah.morris9@gmail.com>
2018-05-28 09:33:33 -0500

    Changed port to work with heroku

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.use((err, req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("server listening on port 3000");
+app.listen(8080, () => {
+  console.log("server listening on port 80");
 });
